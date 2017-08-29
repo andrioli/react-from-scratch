738f5d93d540a8a6b908460179a23effba4da015
Roberto Araujo <roberto.andrioli@gmail.com>
2017-08-29 16:46:41 -0300

    Put bundle.js at the bottom of <body>

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './Hello';
 
-document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(
-    <Hello name='React'/>,
-    document.getElementById('root')
-  );
-});
+ReactDOM.render(
+  <Hello name='React'/>,
+  document.getElementById('root')
+);
