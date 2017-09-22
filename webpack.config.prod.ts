04ca83f3f27632aaa9918fa9075b88f3247f6b41
Roberto Araujo <roberto.andrioli@gmail.com>
2017-09-22 15:15:28 -0300

    Webpack new configuration for production
    Just copied the default webpack file

---
+++ webpack.config.prod.ts
@@ -0,0 +1,52 @@
+import * as HtmlWebpackPlugin from "html-webpack-plugin";
+import * as path from "path";
+import * as webpack from "webpack";
+
+const config: webpack.Configuration = {
+  context: path.join(__dirname, "src"),
+  entry: [
+    "./main.tsx",
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        enforce: "pre",
+        use: [
+          "tslint-loader",
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          "ts-loader",
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", "css-loader", "sass-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(__dirname, "public/index.html"),
+    }),
+  ],
+};
+
+export default config;
