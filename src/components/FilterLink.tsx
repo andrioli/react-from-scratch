0b673b3c38fb00726b76e766d42477f1decd48fa
Roberto Araujo <roberto.andrioli@gmail.com>
2018-01-04 11:45:01 -0200

    Refactor FilterLink container component

--- src/components/FilterLink.tsx
+++ src/components/FilterLink.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import * as ReactRedux from 'react-redux';
 import * as Redux from 'redux';
 import TodoApp from '../models/TodoApp';
@@ -7,35 +6,17 @@ import Link from './Link';
 
 interface FilterLinkProps {
   filter: VisibilityFilter;
-  currentFilter: VisibilityFilter;
-  onClick: (filter: VisibilityFilter) => void;
 }
 
-const FilterLink: React.SFC<FilterLinkProps> = ({
-  filter,
-  currentFilter,
-  onClick,
-  children,
-}) => {
-  return (
-    <Link
-      active={filter === currentFilter}
-      onClick={() => onClick(filter)}
-    >
-      {children}
-    </Link>
-  );
-};
-
-const mapStateToProps = (state: TodoApp) => ({
-  currentFilter: state.visibilityFilter,
+const mapStateToProps = (state: TodoApp, ownProps: FilterLinkProps) => ({
+  active: ownProps.filter === state.visibilityFilter,
 });
 
-const mapDispatchToProps = (dispatch: Redux.Dispatch<TodoApp>) => ({
-  onClick(filter: VisibilityFilter) {
+const mapDispatchToProps = (dispatch: Redux.Dispatch<TodoApp>, ownProps: FilterLinkProps) => ({
+  onClick() {
     dispatch({
       type: 'SET_VISIBILITY_FILTER',
-      filter,
+      filter: ownProps.filter,
     });
   },
 });
@@ -43,4 +24,4 @@ const mapDispatchToProps = (dispatch: Redux.Dispatch<TodoApp>) => ({
 export default ReactRedux.connect(
   mapStateToProps,
   mapDispatchToProps,
-)(FilterLink);
+)(Link);
