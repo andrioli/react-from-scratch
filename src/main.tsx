e3ba02ac7368fd99e55e6dcae261ae856f152f3e
Roberto Araujo <roberto.andrioli@gmail.com>
2018-01-05 14:52:43 -0200

    Persisting the State to the Local Storage

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import throttle = require('lodash/throttle');
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as ReactRedux from 'react-redux';
@@ -5,10 +6,18 @@ import * as Redux from 'redux';
 import * as ReduxDevTools from 'redux-devtools-extension/developmentOnly';
 import * as ReduxThunk from 'redux-thunk';
 import App from './components/App';
+import { loadState, saveState } from './localStorage';
+import { TodoApp } from './models';
 import todoApp from './reducers';
 
-const store = Redux.createStore(todoApp,
+const store = Redux.createStore<TodoApp>(todoApp, loadState(),
   ReduxDevTools.composeWithDevTools(Redux.applyMiddleware(ReduxThunk.default)));
+
+store.subscribe(throttle(() => {
+  saveState({
+    todos: store.getState().todos,
+  });
+}, 1000));
 
 ReactDOM.render(
   <ReactRedux.Provider store={store}>
