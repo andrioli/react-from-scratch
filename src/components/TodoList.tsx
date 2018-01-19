8e53b71a5b586183b34e43c6f78e3a270dbd336b
Roberto Araujo <roberto.andrioli@gmail.com>
2018-01-18 23:52:24 -0200

    Implement toggle all dispatch

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,23 +5,37 @@ import TodoComponent from './Todo';
 interface TodoListProps {
   todos: Todo[];
   onTodoClick: (id: string) => void;
+  onToggleAllClick: () => void;
 }
+
+const isAllCompleted = (todos: Todo[]) => todos.map((t) => t.completed).indexOf(false) < 0;
 
 const TodoList = ({
   todos,
   onTodoClick,
+  onToggleAllClick,
 }: TodoListProps) => (
-  <ul className="todo-list">
-    {
-      todos.map((todo) =>
-        <TodoComponent
-          key={todo.id}
-          onClick={() => onTodoClick(todo.id)}
-          {...todo}
-        />,
-      )
-    }
-  </ul>
+  <div>
+    <input
+      id="toggle-all"
+      className="toggle-all"
+      type="checkbox"
+      checked={isAllCompleted(todos)}
+      onClick={onToggleAllClick}
+    />
+    <label htmlFor="toggle-all">Mark all as complete</label>
+    <ul className="todo-list">
+      {
+        todos.map((todo) =>
+          <TodoComponent
+            key={todo.id}
+            onClick={() => onTodoClick(todo.id)}
+            {...todo}
+          />,
+        )
+      }
+    </ul>
+  </div>
 );
 
 export default TodoList;
--- src/components/VisibleTodoList.tsx
+++ src/components/VisibleTodoList.tsx
@@ -1,7 +1,7 @@
 import * as ReactRedux from 'react-redux';
 import * as ReactRouter from 'react-router';
 import * as Redux from 'redux';
-import { toggleTodo } from '../actions';
+import { toggleAll, toggleTodo } from '../actions';
 import { Todo } from '../models';
 import { getVisibleTodos } from '../reducers';
 import TodoList from './TodoList';
@@ -17,6 +17,9 @@ const mapStateToProps = (state: Todo[], ownProps: VisibleTodoListProps) => ({
 const mapDispatchToProps = (dispatch: Redux.Dispatch<Todo[]>) => ({
   onTodoClick(id: string) {
     dispatch(toggleTodo(id));
+  },
+  onToggleAllClick() {
+    dispatch(toggleAll());
   },
 });
 
