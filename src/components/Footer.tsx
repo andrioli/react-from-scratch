5174f32c10f7179993819dfe520e70bf6611f1ce
Roberto Araujo <roberto.andrioli@gmail.com>
2018-01-19 02:12:07 -0200

    Implement clear completed dispatch

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as ReactRedux from 'react-redux';
 import * as ReactRouter from 'react-router';
+import * as Redux from 'redux';
+import { clearCompleted } from '../actions';
 import { Todo, VisibilityFilter } from '../models';
 import { getVisibleTodos } from '../reducers';
 import FilterLink from './FilterLink';
@@ -8,9 +10,16 @@ import FilterLink from './FilterLink';
 interface FooterProps extends ReactRouter.RouteComponentProps<{}> {
   hidden: boolean;
   todosLeft: number;
+  showClearCompletedButton: boolean;
+  onClickClearCompleted: () => void;
 }
 
-const Footer = ({ hidden, todosLeft }: FooterProps) => (
+const Footer = ({
+  hidden,
+  todosLeft,
+  showClearCompletedButton,
+  onClickClearCompleted,
+}: FooterProps) => (
   <footer className="footer" hidden={hidden}>
     <span className="todo-count"><strong>{todosLeft}</strong> {todosLeft === 1 ? 'item' : 'items'} left</span>
     <ul className="filters">
@@ -36,14 +45,31 @@ const Footer = ({ hidden, todosLeft }: FooterProps) => (
         </FilterLink>
       </li>
     </ul>
+    <button
+      className="clear-completed"
+      hidden={!showClearCompletedButton}
+      onClick={onClickClearCompleted}
+    >
+      Clear completed
+    </button>
   </footer>
 );
 
 const mapStateToProps = (state: Todo[]) => ({
   hidden: state.length === 0,
   todosLeft: getVisibleTodos(state, VisibilityFilter.ShowActive).length,
+  showClearCompletedButton: getVisibleTodos(state, VisibilityFilter.ShowCompleted).length > 0,
+});
+
+const mapDispatchToProps = (dispatch: Redux.Dispatch<Todo[]>) => ({
+  onClickClearCompleted() {
+    dispatch(clearCompleted());
+  },
 });
 
 export default ReactRouter.withRouter(
-  ReactRedux.connect(mapStateToProps)(Footer),
+  ReactRedux.connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(Footer),
 );
